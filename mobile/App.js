a43e67aa4b0a4ee877144aa59b20e45d719968dd
mhmdmaani <mohammedmaani@hotmail.com>
2024-10-27 18:02:11 +0100

    return shared locally

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { ReduxProvider, store } from './src/shared';
 import Main from './src/nav/Main';
 import { RootSiblingParent } from 'react-native-root-siblings';
@@ -13,12 +11,3 @@ export default function App() {
     </ReduxProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
